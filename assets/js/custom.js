7f6da5c55f5026a4729f7ad6703b1126ed92ba8f
Rohan Kapoor <rohanneilkapoor@gmail.com>
2015-09-12 15:12:12 -0700

    updated faqs page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -169,5 +169,26 @@
 			});
 
 	});
+// Find the toggles and hide their content
+	$('.toggle').each(function(){
+		$(this).find('.toggle-content').hide();
+	});
+
+	// When a toggle is clicked (activated) show their content
+	$('.toggle a.toggle-trigger').click(function(){
+		var el = $(this), parent = el.closest('.toggle');
+
+		if( el.hasClass('active') )
+		{
+			parent.find('.toggle-content').slideToggle();
+			el.removeClass('active');
+		}
+		else
+		{
+			parent.find('.toggle-content').slideToggle();
+			el.addClass('active');
+		}
+		return false;
+	});
 
 })(jQuery);
